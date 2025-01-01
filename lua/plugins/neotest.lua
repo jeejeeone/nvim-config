8c57a83f6a7dc5eb560faa6c5a336f9590297d38
jeejeeone <jarijoki1@gmail.com>
2025-01-01 19:37:42 +0200

    LazyVim based config

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-neotest/neotest-python",
+  },
+  config = function()
+    require("neotest").setup({
+      adapters = {
+        require("neotest-python")({}),
+      },
+      output = {
+        enabled = true,
+        open_on_run = "short",
+      },
+      output_panel = {
+        enabled = true,
+        open = "botright split | resize 15",
+      },
+    })
+  end,
+}
---
+++ lua/plugins/noice.lua
@@ -0,0 +1,14 @@
+return {
+  { "rcarriga/nvim-notify", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
+
+  -- classic cmdline
+  {
+    "folke/noice.nvim",
+    opts = {
+      cmdline = {
+        view = "cmdline",
+      },
+    },
+  },
+}

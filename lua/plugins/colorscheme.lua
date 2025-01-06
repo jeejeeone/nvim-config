2726145433cb5623d35bfcca8b5a54a8e1e296ac
jeejeeone <jarijoki1@gmail.com>
2025-01-06 22:49:26 +0200

    add colorcolumn, include lock

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,15 @@
 return {
-  { "rebelot/kanagawa.nvim" },
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      dimInactive = false,
+      theme = "wave", -- Load "wave" theme when 'background' option is not set
+      background = { -- map the value of 'background' option to a theme
+        dark = "wave", -- try "dragon" !
+        light = "lotus",
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
